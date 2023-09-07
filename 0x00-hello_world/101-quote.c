5fe0d39617c1ff81e721091d6fc4d7bbc910a4e7
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-07 18:07:57 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - a function containing the code to be executed,  that prints 
- * exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed 
- * by a new line, to the standard error.
- * Return: 1 if the program executed successfully and did what 
+ * main - a function containing the code to be executed,  that prints
+ * exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: 1 if the program executed successfully and did what
  * it was intended to do
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  
-  return (1);
+
+  	return (1);
 }
