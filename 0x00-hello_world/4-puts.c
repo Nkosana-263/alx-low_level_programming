24776c4e5054f1aea6bb6b28a240eb730b695035
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-07 17:54:18 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - a function containing the code to be executed, that 
+ * main - a function containing the code to be executed, that
  * prints exactly "Programming is like building
  * a multilingual puzzle, followed by a new line
- * Return: 0 if the program executed successfully and did what 
+ * Return: 0 if the program executed successfully and did what
  * it was intended to do
  */
 
