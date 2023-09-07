46a0491b6294248e2b61cc467d55312f72b15841
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-07 17:56:02 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
- * a C program that prints exactly with proper grammar, but the outcome 
+ * main - a function containing the code to be executed, that 
+ * that prints exactly with proper grammar, but the outcome 
  * is a piece of art,, followed by a new line
+ * Return: 0 if the program executed successfully and did what 
+ * it was intended to do
  */
 
 int main(void)
