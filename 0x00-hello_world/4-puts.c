c6c671e7732e0f572b6dbb798805efa42d9e308e
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-07 17:47:50 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  a c program that prints exactly "Programming is like building 
+ * a c program that prints exactly "Programming is like building
  * a multilingual puzzle, followed by a new line
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
