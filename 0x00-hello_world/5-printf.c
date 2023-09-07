1f5b7a62694de441f4a0e1bf3f9cc7338f3ed487
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-07 17:57:25 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - a function containing the code to be executed, that 
+ * main - a function containing the code to be executed, that
  * that prints exactly with proper grammar, but the outcome 
  * is a piece of art,, followed by a new line
- * Return: 0 if the program executed successfully and did what 
+ * Return: 0 if the program executed successfully and did what
  * it was intended to do
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-	return 0;
+
+	return (0);
 }
