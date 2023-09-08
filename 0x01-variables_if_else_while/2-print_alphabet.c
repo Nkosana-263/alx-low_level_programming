2fda68f2a755b2dbb5813013d62a060dfe9b865a
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-08 17:25:36 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-    char letter;
-
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-
-    putchar('\n');
-
-    return (0);
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	
+	putchar('\n');
+	
+	return (0);
 }
