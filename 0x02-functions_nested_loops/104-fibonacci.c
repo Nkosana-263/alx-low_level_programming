1ca060dce176e9e931db39323fb4a8f8558bd814
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 15:25:27 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,35 +8,33 @@
  */
 int main(void)
 {
-	unsigned long int fib1 = 1;
-	unsigned long int fib2 = 2;
-	unsigned long int i, nextFib, j1, j2, k1, k2;
+	unsigned long int i, fib1_temp1, fib1_temp2, fib2_temp1, fib2_temp2;
 
-	printf("%lu, ", fib1);
-	printf("%lu", fib2);
+	usigned long int fib1 = 1;
+	unsigned long int fib2 = 2;
+
+	printf("%lu", fib1);
 
 	for (i = 1; i < 91; i++)
 	{
-		nextFib = fib1 + fib2;
-		fib1 = fib2;
-		fib2 = nextFib;
-
-		printf(", %lu", nextFib);
+		printf(", %lu", k);
+		fib2 = fib2 + fib1;
+		fib1 = fib2 - fib1;
 	}
 
-	j1 = fib1 / 1000000000;
-	j2 = fib1 % 1000000000;
-	k1 = fib2 / 1000000000;
-	k2 = fib2 % 1000000000;
+	fib1_temp1 = fib1 / 1000000000;
+	fib1_temp2 = fib1 % 1000000000;
+	fib2_temp1 = fib2 / 1000000000;
+	fib2_temp2 = fib2 % 1000000000;
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu", k1 + (k2 / 1000000000));
+		printf(", %lu", fib2_temp1 + (fib2_temp2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
+		fib2_temp1 = fib2_temp1 + fib1_temp1;
+		fib1_temp1 = fib2_temp1 - fib1_temp1;
+		fib2_temp2 = fib2_temp2 + fib1_temp2;
+		fib1_temp2 = fib2_temp2 - fib1_temp2;
 	}
 
 	printf("\n");
