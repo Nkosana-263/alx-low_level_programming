2ddb4452f514f92291c21310e0227b8844fd0877
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 15:28:58 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,7 @@ int main(void)
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %lu", k);
+		printf(", %lu", fib2);
 		fib2 = fib2 + fib1;
 		fib1 = fib2 - fib1;
 	}
@@ -30,7 +30,7 @@ int main(void)
 	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", fib2_temp1 + (fib2_temp2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
+		printf("%lu", fib2_temp2 % 1000000000);
 		fib2_temp1 = fib2_temp1 + fib1_temp1;
 		fib1_temp1 = fib2_temp1 - fib1_temp1;
 		fib2_temp2 = fib2_temp2 + fib1_temp2;
