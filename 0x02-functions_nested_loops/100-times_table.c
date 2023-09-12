e743970279f66801df3a07afdf73a884d58a1dcc
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 13:34:21 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,8 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 				}
 			} else if (result < 100)
 			{
@@ -46,8 +48,6 @@ void print_times_table(int n)
 				if (j != n)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
