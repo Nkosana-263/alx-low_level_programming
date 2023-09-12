c3fb1815ae278b36508fc19f0b1bc3ba78fe74b0
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 13:06:52 +0200

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main"
+
+/**
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number of times
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		return;
+
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
+			if (result < 10)
+			{
+				_putchar(result + '0');
+			} else if (result < 100)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			} else
+			{
+				_putchar((result / 100) + '0');
+				_putchar(((result / 10) % 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
