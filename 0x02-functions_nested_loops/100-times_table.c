f016a6f29cfd3f3ad7a4389e61730714294d45ac
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 13:39:57 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,61 +10,42 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n > 15 || n < 0)
+	if (n < 0 || n > 15)
 		return;
 
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= n; j++)
+		_putchar('0'); // Print the first digit
+
+		for (j = 1; j <= n; j++)
 		{
 			int result = i * j;
+			_putchar(','); // Print comma before each number
+			
+			// Print spaces to align numbers properly
 			if (result < 10)
 			{
-				_putchar(result + '0');
-				if (j != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(' ');
 			} else if (result < 100)
 			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-
-				if (j != n)
-				{	
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			} else
+				_putchar(' ');
+			}
+			
+			// Print the two or three-digit number
+			if (result >= 100)
 			{
 				_putchar((result / 100) + '0');
 				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
-
-				if (j != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-
-			/* if (j != n)
+			} else if (result >= 10)
 			{
-				_putchar(',');
-				_putchar(' ');
-				
-				if (result <= 100)
-					_putchar(' ');
-				if (result <= 10)
-					_putchar(' ');
+				_putchar((result / 10) + '0');
 			}
-			*/
 
+			_putchar((result % 10) + '0'); // Print the last digit
 		}
-		_putchar('\n');
+
+		_putchar('\n'); // Move to the next line after each row is printed
 	}
 }
+
