b0e09c2b976b10d6c77941353da0f3a3f963199d
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 12:25:47 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,13 +12,13 @@ int print_last_digit(int num)
 
 	if (num < 0)
 		num *= -1;  /* Convert negative number to positive */
-	
+
 	last_digit = num % 10;
 
 	if (last_digit < 0)
 		last_digit *= -1;  /* Convert negative number to positive */
-	
+
 	_putchar(last_digit + '0');
-	
+
 	return (last_digit);
 }
