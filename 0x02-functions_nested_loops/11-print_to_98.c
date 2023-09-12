493bd4a89843430f430d26b25cb2afb7ddaf8dd3
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 13:01:30 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,17 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98) 
+	if (n <= 98)
 	{
-		for (; n <= 98; n++) {
+		for (; n <= 98; n++)
+		{
 			printf("%d", n);
 			if (n != 98)
 				printf(", ");
 		}
-	} else 
+	} else
 	{
-		for (; n >= 98; n--) 
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 			if (n != 98)
