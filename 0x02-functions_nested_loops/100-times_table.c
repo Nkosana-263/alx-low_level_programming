85d148caafba103f342bd2c19e55c35de05a555c
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 13:51:04 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,44 +9,29 @@
 void print_times_table(int n)
 {
 	int i, j;
-
-	if (n < 0 || n > 15)
-		return;
-
-	for (i = 0; i <= n; i++)
-	{
+	
+	if (n < 0 || n > 15) return;
+	for (i = 0; i <= n; i++) {
 		_putchar('0');
-
 		for (j = 1; j <= n; j++)
 		{
 			int result = i * j;
-
+			
 			_putchar(',');
-
-			if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			} else if (result < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-
+			_putchar(' ' + (result < 10));
+			_putchar(' ' + ((result < 100) && (result >= 10)));
+			
 			if (result >= 100)
 			{
 				_putchar(' ');
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-			} else if (result >= 10)
+				_putchar(result / 100 + '0');
+				_putchar((result / 10) % 10 + '0');
+			} else if (result >= 10) 
 			{
-				_putchar((result / 10) + '0');
+				_putchar(result / 10 + '0');
 			}
-
-			_putchar((result % 10) + '0');
+			_putchar(result % 10 + '0');
 		}
-
 		_putchar('\n');
 	}
 }
