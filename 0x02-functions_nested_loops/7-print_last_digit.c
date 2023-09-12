b73dbe089100463e07d668c0429ded324ee59f74
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-12 12:21:34 +0200

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @num: The number to extract the last digit from.
+ *
+ * Return: The value of the last digit.
+ */
+int print_last_digit(int num)
+{
+	int last_digit;
+
+	if (num < 0)
+		num *= -1;  /* Convert negative number to positive */
+	
+	last_digit = num % 10;
+	
+	_putchar(last_digit + '0');
+	
+	return (last_digit);
+}
