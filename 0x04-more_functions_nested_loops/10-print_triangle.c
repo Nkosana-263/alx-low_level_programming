3df4b2ed1149770bd1e658c06a99afb981c56791
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-14 13:25:27 +0200

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,20 +6,22 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		for (j = 0; j < size - i - 1; j++)
-			_putchar(' ');
-		for (j = 0; j <= i; j++)
-			_putchar('#');
-		_putchar('\n');
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+				_putchar(' ');
+			for (j = 0; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
