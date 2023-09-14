ccdf4d9c0d28988244ca2f34c9759533d5e50170
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-14 13:31:37 +0200

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,20 @@
  */
 void print_number(int n)
 {
+	unsigned int n_pos;
+	
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n_pos = -n;
+	}
+	else
+	{
+		n_pos = n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (n_pos / 10 == 0)
+		print_number(n_pos / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((n_pos % 10) + '0');
 }
