0a138a0947bd3cd85593521095bdad869563584e
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-14 12:06:38 +0200

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ * print_numbers - Prints the numbers from 0 to 9,
+ * followed by a new line.
  */
 void print_numbers(void)
 {
