944e810cd9c7944b48e74a249e1c75a116727298
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-18 19:29:37 +0200

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,26 +9,18 @@
 int _atoi(char *s)
 {
 	int sign = 1;
-	int result = 0;
+	unsigned int result = 0;
 	int i = 0;
 
-	if (s[0] == '-')
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-		sign = -1;
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
-
-	while (s[i] != '\0')
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (result > 0)
-		{
-			break;
-		}
-
+		result = (result * 10) + (s[i] - '0');
 		i++;
 	}
 
