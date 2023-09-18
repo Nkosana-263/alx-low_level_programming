b7c624ff254de87e7bd8ab73f8dcb9ae8602f71d
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-18 19:12:33 +0200

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string, starting with the first character.
+ * @str: Pointer to a string.
+ *
+ * Description: This function takes a pointer to a string as a parameter
+ * and prints every other character of the string, starting with the first character.
+ * It iterates through the characters of the string and uses an if statement to check
+ * if the current index is even. If it is, it calls _putchar to print that character.
+ * After printing all characters, it calls _putchar again to print a new line character.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+
+		i++;
+	}
+
+	_putchar('\n');
+}
