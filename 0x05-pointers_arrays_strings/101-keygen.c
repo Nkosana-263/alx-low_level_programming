c1fbc72c42ed9e4df883c8d841b49364a377f3cb
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-18 19:36:23 +0200

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,19 +9,31 @@
  */
 int main(void)
 {
-	char password[100];
-	int i;
-
-	srand(time(0));
-
-	for (i = 0; i < 100; i++)
-	{
-		password[i] = rand() % 26 + 'a';
-	}
-
-	password[99] = '\0';
-
-	printf("%s\n", password);
-
-	return 0;
+    int ascii = 2772;
+    int i = 0;
+    int j;
+    int random;
+    char password[100];
+    
+    srand(time(NULL));
+    
+    while (ascii > 126)
+    {
+        random = rand() % 126;
+        password[i] = (char)random;
+        ascii -= random;
+        i++;
+    }
+    
+    if (ascii > 0)
+        password[i] = (char)ascii;
+    else
+        i--;
+    
+    for (j = 0; j <= i; j++)
+        putchar(password[j]);
+    
+    putchar('\n');
+    
+    return (0);
 }
