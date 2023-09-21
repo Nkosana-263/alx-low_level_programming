b6c9a25c539be529d2d301e3d2dc956d93dd02ee
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-21 11:50:42 +0200

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 algorithm.
+ * @str: The string to be encoded.
+ *
+ * Return: The encoded string.
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	int i;
+
+	while (*ptr != '\0')
+	{
+		if ((*ptr >= 'a' && *ptr <= 'm') || (*ptr >= 'A' && *ptr <= 'M'))
+		{
+			*ptr += 13;
+		}
+		else if ((*ptr >= 'n' && *ptr <= 'z') || (*ptr >= 'N' && *ptr <= 'Z'))
+		{
+			*ptr -= 13;
+		}
+		ptr++;
+	}
+
+	return (str);
+}
