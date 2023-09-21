618875be00e96bc96b7a06d919c3423a91b29ccb
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-21 12:40:27 +0200

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * rev_string - Reverses array.
+ * @n: integer params.
+ * Return: 0
+ */
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
  * infinite_add - Adds two numbers.
  * @n1: The first number as a string.
  * @n2: The second number as a string.
@@ -11,38 +36,42 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0;
-	int i, j;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-
-	if (len1 > size_r || len2 > size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-
-	r[size_r - 1] = '\0';
-	i = len1 - 1;
-	j = len2 - 1;
-
-	while (i >= 0 || j >= 0)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (i >= 0)
-			carry += n1[i--] - '0';
-		if (j >= 0)
-			carry += n2[j--] - '0';
-
-		r[--size_r] = carry % 10 + '0';
-		carry /= 10;
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-
-	if (carry)
-	{
-		if (size_r == 1)
-			return 0;
-		r[--size_r] = carry + '0';
-	}
-
-	return (&r[size_r]);
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
