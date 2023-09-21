c5468e56e4747e40c0698282f346b5383813c51d
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-21 11:36:43 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,6 @@ char *cap_string(char *str)
 {
 	int i;
 
-	if (str[0] >= 'a' && str[0] <= 'z')
-		str[0] -= 32;
-
 	for (i = 1; str[i] != '\0'; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
