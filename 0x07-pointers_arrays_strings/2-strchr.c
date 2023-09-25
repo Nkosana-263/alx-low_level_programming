8cf4071a6d756fdf72508c8f33f4e4ce058f37b0
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-25 14:35:28 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,9 +5,10 @@
  * @s: Pointer to the string to be searched.
  * @c: The character to be located.
  *
- * Return: A pointer to the first occurrence of
- * 	   the character c in the string s,
- *         or NULL if the character is not found.
+ * Return: A pointer to the first occurrence
+ *         of the character c in the string
+ *         s, or NULL if the character is
+ *	   not found.
  */
 char *_strchr(char *s, char c)
 {
