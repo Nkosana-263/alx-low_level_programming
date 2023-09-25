25f134e8a7d3f88005783007f3caa697ca2f7ddd
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-25 14:32:39 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (true)
+	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
@@ -20,5 +20,5 @@ char *_strchr(char *s, char c)
 	if (c == '\0')
 		return (s);
 
-	return (NULL);
+	return (0);
 }
