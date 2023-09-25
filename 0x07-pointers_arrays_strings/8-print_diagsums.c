e561e8eb90d34185c7d26b259cbd44b2061c9c6b
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-25 14:50:03 +0200

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - Prints the sum of the two
@@ -10,9 +11,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1 = 0;
-	int sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
