a67d1210c3b1c3f180ba5b0bfa7fe2fc0f75826e
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-09-28 18:16:26 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,10 +10,10 @@ int prime_helper(int n, int i);
  */
 int is_prime_number(int n)
 {
-        if (n <= 1)
-                return (0);
+	if (n <= 1)
+		return (0);
 
-        return (prime_helper(n, 2));
+	return (prime_helper(n, 2));
 }
 
 /**
@@ -25,11 +25,11 @@ int is_prime_number(int n)
  */
 int prime_helper(int n, int i)
 {
-        if (i == n)
-                return (1);
+	if (i == n)
+		return (1);
 
-        if (n % i == 0)
-                return (0);
+	if (n % i == 0)
+		return (0);
 
-        return (prime_helper(n, i + 1));
+	return (prime_helper(n, i + 1));
 }
