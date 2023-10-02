5e9b2fa2ba108b139645c82529c4c7438c834d7d
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-02 14:45:22 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,12 @@
 
 /**
  * main - Entry point
- * @argc: The number of arguments supplied to the program.
+ * @argc: The number of arguments supplied to
+ *        the program.
  * @argv: An array of pointers to the arguments.
  *
- * Description: Prints the program name, followed by a new line.
+ * Description: Prints the program name, followed
+ *              by a new line.
  * Return: Always 0 (Success).
  */
 int main(int argc __attribute__((unused)), char *argv[])
