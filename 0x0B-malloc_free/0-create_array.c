da0f9a046395c5b8ee0de132e7e9d89c8af43ff2
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-04 23:36:25 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,11 +13,12 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *array
 
 	if (size == 0)
 		return (NULL);
 
-	char *array = malloc(sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 	if (array == (NULL))
 		return (NULL);
 
