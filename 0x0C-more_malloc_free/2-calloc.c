0fce21a30a7d5eb651572c4d7d68ac105e630759
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-08 20:12:49 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,14 +11,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-
 	unsigned int i;
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < n; i++)
+	{
 		s[i] = b;
 	}
 
