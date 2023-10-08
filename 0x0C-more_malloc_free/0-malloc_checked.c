e1d9090a9745b52336515b6dd59191a761bef654
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-08 18:46:58 +0200

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ *
+ * @b: The size of the memory to allocate in bytes.
+ *
+ * Return: A pointer to the allocated memory, or 98 if malloc fails.
+ */
+void *malloc_checked(unsigned int b) {
+	void *ptr = malloc(b);
+
+	if (ptr == NULL) {
+		exit(98);
+	}
+
+	return (ptr);
+}
