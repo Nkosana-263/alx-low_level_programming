f2b64889935689a4d5c6cc2c14dace0588734da3
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-08 20:21:59 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,15 +19,17 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0 && ptr != NULL) {
+	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
-		return NULL;
+		return (NULL);
 	}
 
 	if (ptr == NULL)
 		return (malloc(new_size));
 
 	ptr1 = malloc(new_size);
+
 	if (!ptr1)
 		return (NULL);
 
