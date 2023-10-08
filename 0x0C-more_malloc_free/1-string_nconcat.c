2dadab496d8c38afe4cc2816cc6a4d4fff1f364c
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-08 19:05:44 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,17 +18,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, s1_len = 0, s2_len = 0;
 
 	while (s1 && s1[s1_len])
+	{
 		s1_len++;
+	}
 	while (s2 && s2[s2_len])
+	{
 		s2_len++;
+	}
 
 	if (n < s2_len)
+	{
 		s = malloc(sizeof(char) * (s1_len + n + 1));
+	}
 	else
+	{
 		s = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	}
 
 	if (!s)
+	{
 		return (NULL);
+	}
 
 	while (i < s1_len)
 	{
@@ -37,10 +47,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	while (n < s2_len && i < (s1_len + n))
+	{
 		s[i++] = s2[j++];
+	}
 
 	while (n >= s2_len && i < (s1_len + s2_len))
+	{
 		s[i++] = s2[j++];
+	}
 
 	s[i] = '\0';
 
