3ad8cf403d7715f397d435b5e66ed23b64db81d0
Nkosana Daniel Mlandu <mlandunkosana@gmail.com>
2023-10-10 23:41:01 +0200

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,10 +2,10 @@
 #include "dog.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * _strlen - Returns the length of a string.
+ * @s: String to evaluate
  *
- * Return: the length of the string
+ * Return: The length of the string
  */
 int _strlen(char *s)
 {
@@ -22,13 +22,13 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcpy - copies the string pointed to by src
+ * *_strcpy - Copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
+ * @dest: Pointer to the buffer in which we copy the string
+ * @src: String to be copied
  *
- * Return: the pointer to dest
+ * Return: The pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
@@ -51,12 +51,12 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: Pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
